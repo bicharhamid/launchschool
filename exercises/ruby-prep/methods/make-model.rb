26df4d2e10ddd2ad68bdc5fb06d23d83ff84f020
bicharhamid <162973695+bicharhamid@users.noreply.github.com>
2024-08-09 08:32:36 -0500

    Finished exercise

---
+++ exercises/ruby-prep/methods/make-model.rb
@@ -0,0 +1,6 @@
+# Using the following code, write a method called car that takes two arguments and prints a string containing the values of both arguments.
+
+def car(make, model)
+  puts "This car is from #{make} and the model is #{model}"
+end
+
