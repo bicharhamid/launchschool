61b6f46f3e4b11ad8b0580901aa0256536280264
bicharhamid <162973695+bicharhamid@users.noreply.github.com>
2024-08-09 21:16:11 -0500

    Added new folder path

---
+++ exercises/small-problems/documentation-again/class-instance-methods.rb
@@ -0,0 +1,1 @@
+# Locate the ruby documentation for methods File::path and File#path for version 2.7.8 of Ruby here (newer versions of Ruby have changed the implementation of these methods, so be sure to follow the link provided). How are they different?
